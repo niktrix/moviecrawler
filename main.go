96b22a879edea3184b4fa77a79b41d7aba0ef327
Nickyg <nicky.gurbani@gmail.com>
2017-03-27 00:38:47 +0530

    WIP: hotstar website

--- main.go
+++ main.go
@@ -5,106 +5,65 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"strconv"
 	"strings"
 )
 
-type VootResponse struct {
-	Assets []struct {
-		ID          string `json:"id"`
-		Type        int    `json:"type"`
-		Name        string `json:"name"`
-		Description string `json:"description"`
-		Images      []struct {
-			Ratio  string `json:"ratio"`
-			Width  int    `json:"width"`
-			Height int    `json:"height"`
-			URL    string `json:"url"`
-		} `json:"images"`
-		Metas struct {
-			ContentSynopsis string `json:"ContentSynopsis"`
-			ContentType     string `json:"ContentType"`
-			ContentFileName string `json:"ContentFileName"`
-			MovieMainTitle  string `json:"MovieMainTitle"`
-			SBU             string `json:"SBU"`
-			IsDownable      string `json:"IsDownable"`
-			ContentDuration string `json:"ContentDuration"`
-			ReleaseYear     string `json:"ReleaseYear"`
-		} `json:"metas"`
-		Tags struct {
-			Keywords        []string `json:"Keywords"`
-			CharacterList   []string `json:"CharacterList"`
-			ContributorList []string `json:"ContributorList"`
-			Scene1          []string `json:"Scene1"`
-			Scene2          []string `json:"Scene2"`
-			Scene3          []string `json:"Scene3"`
-			Scene4          []string `json:"Scene4"`
-			Scene5          []string `json:"Scene5"`
-			Scene6          []string `json:"Scene6"`
-			Genre           []string `json:"Genre"`
-			Language        []string `json:"Language"`
-			AdCueTime1      []string `json:"AdCueTime1"`
-			AdCueTime2      []string `json:"AdCueTime2"`
-			AdCueTime3      []string `json:"AdCueTime3"`
-			AdCueTime4      []string `json:"AdCueTime4"`
-			AdCueTime5      []string `json:"AdCueTime5"`
-			AdCueTime6      []string `json:"AdCueTime6"`
-			AdCueTime7      []string `json:"AdCueTime7"`
-			AdCueTime8      []string `json:"AdCueTime8"`
-			MediaExternalID []string `json:"MediaExternalId"`
-			WatermarkURL    []string `json:"WatermarkURL"`
-			MovieDirector   []string `json:"MovieDirector"`
-		} `json:"tags"`
-		StartDate   int   `json:"start_date"`
-		EndDate     int64 `json:"end_date"`
-		ExtraParams struct {
-			SysStartDate string      `json:"sys_start_date"`
-			SysFinalDate string      `json:"sys_final_date"`
-			ExternalIds  interface{} `json:"external_ids"`
-			EntryID      string      `json:"entry_id"`
-		} `json:"extra_params"`
-		RURL interface{} `json:"rURL"`
-	} `json:"assets"`
-	TotalItems int `json:"total_items"`
-	Status     struct {
-		Code    int    `json:"code"`
-		Message string `json:"message"`
-	} `json:"status"`
+type webconfig struct {
+	name string
+	url  string
+}
+
+var availableWebsites = []webconfig{
+	{"voot", "https://wapi.voot.com/ws/ott/searchAssets.json?platform=Web&pId=2"},
+	//{"hotstar", "http://search.hotstar.com/AVS/besc"},
 }
 
 type MovieRequester struct {
 	url       string
 	pageIndex string
 	request   *http.Request
+	website   string
 }
 
 func main() {
-	mr := MovieRequester{}
-	mr.url = "https://wapi.voot.com/ws/ott/searchAssets.json?platform=Web&pId=2"
 
-	index := 0
-	totalMovies := 0
-	for {
-		mr.pageIndex = strconv.Itoa(index)
-		b, _ := mr.get(mr.pageIndex)
-		count := mr.unmarshalMovies(b)
-		totalMovies = totalMovies + count
-		if count == 0 {
-			break
+	for _, webs := range availableWebsites {
+		fmt.Println("Getting movies for ", webs.name)
+		mr := MovieRequester{}
+		mr.url = webs.url
+		mr.website = webs.name
+		index := 0
+		totalMovies := 0
+		for {
+			mr.pageIndex = strconv.Itoa(index)
+			b, _ := mr.get(mr.pageIndex)
+			count := mr.unmarshalMovies(b)
+			totalMovies = totalMovies + count
+			if count == 0 {
+				break
+			}
+			index++
+
 		}
-		index++
-
+		fmt.Println(totalMovies)
 	}
-	fmt.Println(totalMovies)
 
 }
 
 func (mr *MovieRequester) get(pageIndex string) ([]byte, error) {
 	//url := "https://wapi.voot.com/ws/ott/searchAssets.json?platform=Web&pId=2"
 	mr.request, _ = mr.requesrUrl()
+	//if mr.website == "voot" {
 	mr.request.Header.Add("content-type", "application/x-www-form-urlencoded")
+	//	}
+
+	dump, _ := httputil.DumpRequestOut(mr.request, true)
+	fmt.Println(string(dump))
 	res, _ := http.DefaultClient.Do(mr.request)
 	defer res.Body.Close()
 	return ioutil.ReadAll(res.Body)
@@ -112,25 +71,67 @@ func (mr *MovieRequester) get(pageIndex string) ([]byte, error) {
 }
 
 func (mr *MovieRequester) unmarshalMovies(b []byte) int {
-	r := VootResponse{}
-	json.Unmarshal(b, &r)
-	for _, movies := range r.Assets {
-		fmt.Println(movies.Name)
+
+	switch mr.website {
+	case "hotstar":
+		r := HotstarResponse{}
+		json.Unmarshal(b, &r)
+		for _, movies := range r.ResultObj.Response.Docs {
+			fmt.Println(movies.ContentTitle + "    " + mr.website)
+		}
+		return len(r.ResultObj.Response.Docs)
+	case "voot":
+		r := VootResponse{}
+		log.Println("Error while ub", string(b))
+
+		err := json.Unmarshal(b, &r)
+		if err != nil {
+			log.Println("Error while unmarshalling voot", err)
+		}
+		log.Println("  voot", r)
+		for _, movies := range r.Assets {
+			fmt.Println(movies.Name + "    " + mr.website)
+		}
+		return len(r.Assets)
 	}
-	return len(r.Assets)
+	return 0
 }
 
 func (mr *MovieRequester) getPostVars() io.Reader {
 	form := url.Values{}
-	form.Add("filterTypes", "390")
-	//form.Add("filter", "(and (and  contentType='Movie' ))")
-	form.Add("pageIndex", mr.pageIndex)
-	form.Add("pageSize", "10")
-	fmt.Println(form.Encode())
+	switch mr.website {
+	case "hotstar":
+		form.Add("action", "SearchContents")
+		form.Add("appVersion", "5.0.40")
+		form.Add("channel", "PCTV")
+		form.Add("maxResult", "12")
+		form.Add("query", "*")
+		form.Add("searchOrder", "counter_day desc")
+		form.Add("startIndex", mr.pageIndex)
+		form.Add("type", "type:MOVIE")
+	case "voot":
+		form := url.Values{}
+		form.Add("filterTypes", "390")
+		//form.Add("filter", "(and (and  contentType='Movie' ))")
+		form.Add("pageIndex", mr.pageIndex)
+		form.Add("pageSize", "10")
+		fmt.Println(form.Encode())
+	}
+
 	return strings.NewReader(form.Encode())
 }
 
 func (mr *MovieRequester) requesrUrl() (*http.Request, error) {
-	return http.NewRequest("POST", mr.url, mr.getPostVars())
+	switch mr.website {
+	case "hotstar":
+		v := "?action=SearchContents&appVersion=5.0.40&channel=PCTV&maxResult=12&moreFilters=language:hindi%3B&query=*&searchOrder=counter_day+desc&startIndex=" + mr.pageIndex + "&type=MOVIE"
+		return http.NewRequest("GET", mr.url+v, nil)
+	case "voot":
+		fmt.Println("vooooooo", mr.url)
 
+		return http.NewRequest("POST", mr.url, mr.getPostVars())
+	default:
+		return http.NewRequest("GET", mr.url, nil)
+	}
+	return nil, nil
 }
